85d1cb5e687a82a94f7353131516ee755e2ed813
darina.Ilenko <dilenko13@gmail.com>
2021-09-30 11:42:26 +0300

    first commit

---
+++ src/main/java/FirstTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    @Test
+
+    public void testGoogle(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+
+
+
+
+}
