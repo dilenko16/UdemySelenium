4635d67fc0bbb0827fba0dae5bb08c9e97784f69
darina.Ilenko <dilenko13@gmail.com>
2021-12-30 17:37:32 +0200

    added attachScreenShotsToAllure

--- src/test/java/tests/base/Listener.java
+++ src/test/java/tests/base/Listener.java
@@ -1,5 +1,6 @@
 package tests.base;
 
+import io.qameta.allure.Attachment;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
@@ -30,6 +31,11 @@ public class Listener implements TestWatcher {
             LOGGER.info("Exception on saving screenshot");
             e.printStackTrace();
         }
+        attachScreenshotsToAllure(ts);
     }
 
+    @Attachment
+    public byte [] attachScreenshotsToAllure(TakesScreenshot takesScreenshot){
+        return takesScreenshot.getScreenshotAs(OutputType.BYTES);
+    }
 }
